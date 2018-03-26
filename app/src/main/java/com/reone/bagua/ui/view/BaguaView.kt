27aa3a756149921e56f73a6fcb6fdd1cec6b2e32
wangxingsheng <164587694@qq.com>
2018-03-26 15:41:01 +0800

    阴阳图可以控制起始角度，可实现旋转功能

--- app/src/main/java/com/reone/bagua/ui/view/BaguaView.kt
+++ app/src/main/java/com/reone/bagua/ui/view/BaguaView.kt
@@ -1,6 +1,5 @@
 package com.reone.bagua.ui.view
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -8,12 +7,20 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import com.reone.bagua.core.util.LOG
+import kotlin.math.cos
+import kotlin.math.sin
 
 /**
  * Created by wangxingsheng on 2018/3/26.
  *
  */
 class BaguaView: View {
+    private val TAG = "BaguaView"
+    companion object {
+        var start = 0f
+    }
+    private var rotating = false
     constructor(context: Context?) : this(context,null)
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs,0)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
@@ -22,14 +29,18 @@ class BaguaView: View {
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        drawYinyang(canvas,0f,width.toFloat(),0f,height.toFloat())
+        drawYinyang(canvas,0f,width.toFloat(),0f,height.toFloat(), start)
+        if(rotating){
+            start = (start + 1f)%360f
+            invalidate()
+        }
     }
 
     /**
      * 阴阳图
+     * @param start 基准角度，x轴正方向为0
      */
-    @SuppressLint("NewApi")
-    private fun drawYinyang(canvas: Canvas, l:Float, r:Float, t:Float, b:Float) {
+    private fun drawYinyang(canvas: Canvas, l:Float, r:Float, t:Float, b:Float,start:Float) {
         //初始阴阳画笔
         val yinPaint = Paint()
         yinPaint.color = Color.BLACK
@@ -43,13 +54,28 @@ class BaguaView: View {
         yangPaint.isDither = true
         //阴阳底色
         val yinyang = RectF(l,t,r,b)
-        canvas.drawArc(yinyang,-90f,180f,false,yinPaint)
-        canvas.drawArc(yinyang,90f,180f,false,yangPaint)
+        canvas.drawArc(yinyang,start-90f,180f,false,yinPaint)
+        canvas.drawArc(yinyang,start+90f,180f,false,yangPaint)
+        //阴阳弧半径
+        val r1 = ( b - t ) / 4
+        //阴阳眼半径
+        val r2 = r1 / 4
+        //阴阳底圆心
+        val x = ( r + l ) / 2
+        val y = ( b + t ) / 2
+        //起始偏移角度
+        val angle = start/180*Math.PI.toFloat()
+        //阴眼圆心
+        val x1 = r1 * sin(angle) + x
+        val y1 = y - r1 * cos(angle)
+        //阳眼圆心
+        val x2 = x - r1 * sin(angle)
+        val y2 = r1 * cos(angle) + y
         //阴阳弧
-        canvas.drawCircle(r/2,b/4*3,b/4,yinPaint)
-        canvas.drawCircle(r/2,b/4,b/4,yangPaint)
+        canvas.drawCircle(x2,y2,r1,yinPaint)
+        canvas.drawCircle(x1,y1,r1,yangPaint)
         //阴阳眼
-        canvas.drawCircle(r/2,b/4,b/16,yinPaint)
-        canvas.drawCircle(r/2,b/4*3,b/16,yangPaint)
+        canvas.drawCircle(x1,y1,r2,yinPaint)
+        canvas.drawCircle(x2,y2,r2,yangPaint)
     }
 }
